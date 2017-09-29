75a9555e200222ca3c7d7c5a31949e640bc44618
Rafael Furry <rfurry@gmail.com>
2017-09-29 11:39:36 -0700

    create brands table

--- db/migrate/20170929175834_create_brands.rb
+++ db/migrate/20170929175834_create_brands.rb
@@ -1,5 +1,10 @@
 class CreateBrands < ActiveRecord::Migration[5.1]
   def change
-    
+    create_table :brands do |t|
+      t.column :name, :string
+      t.column :price, :string
+
+      t.timestamps
+    end
   end
 end
