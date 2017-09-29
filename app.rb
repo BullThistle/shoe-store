12817a42a7b62ab7ba452a013fb5078517f5ff2c
Rafael Furry <rfurry@gmail.com>
2017-09-29 13:23:55 -0700

    create gets for all erbs

--- app.rb
+++ app.rb
@@ -7,3 +7,23 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get '/' do
   erb :index
 end
+
+get '/brands' do
+
+  erb :brands
+end
+
+get '/stores' do
+
+  erb :stores
+end
+
+get '/add_brand' do
+
+  erb :add_brand
+end
+
+get '/add_store' do
+
+  erb :add_store
+end
