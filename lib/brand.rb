cabff2855ce5ac984d6ad9f4ad9a732fcbf04d39
Rafael Furry <rfurry@gmail.com>
2017-09-29 11:36:57 -0700

    create validations and callbacks for brand

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,14 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many :stores
+
+  validates_presence_of :name
+  validates_presence_of :price
+
+  before_save(:upcase_name)
+  before_save(:upcase_name)
+
+  private
+    def upcase_name
+      self.name.split(/ |\_/).map(&:capitalize).join(" ")
+    end
 end
